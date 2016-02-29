a3973c8e545654ee90b960e2394bf7eda52cba35
Christine <christinebeaubrun@gmail.com>
2016-02-28 17:01:16 -0800

    Added test for exhibition model

--- test/models/exhibition_test.rb
+++ test/models/exhibition_test.rb
@@ -1,21 +1,20 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class ExhibitionTest < ActiveSupport::TestCase
-#   def setup
-#   	@exhibitionFalse = Exhibition.new(name: "The Bold and the Brave")
-#   	@exhibition = Exhibition.new(
-#   		name: "The Bold and the Brave",
-#   		exhibit_date: "March 28 - August 1",
-#   		about_info: "A show about the brave and the bold",
-#   		img_url: 'https:www.google.com/image/wonders_of_the_world_a_second_time'
-#   	)
-#   end
+class ExhibitionTest < ActiveSupport::TestCase
+  def setup
+  	@exhibitionFalse = Exhibition.new()
+  	@exhibit1 = exhibitions(:exhibit1)
+  end
 
-#   test "should not be valid if missing any form fields for exhibition" do
-#   	assert_not @exhibitionFalse.valid?
-#   end
+  test "should not be valid if missing any form fields for exhibition" do
+  	assert_not @exhibitionFalse.valid?
+  end
 
-#   test "should validate exhibition if all options are provided" do
-#   	assert @exhibition.valid?
-#   end
-# end
+  test "should validate exhibition if all options are provided" do
+  	assert @exhibit1.valid?
+  end
+
+  test "should have belongs_to museum association" do
+  	assert_equal "Museum of Modern Art", @exhibit1.museum.name
+  end
+end
