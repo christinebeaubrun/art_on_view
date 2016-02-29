11771c1de8f5d8d885231f883631f832e904a499
Christine <christinebeaubrun@gmail.com>
2016-02-28 16:50:46 -0800

    Added test for museum model

--- test/models/museum_test.rb
+++ test/models/museum_test.rb
@@ -1,17 +1,27 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class MuseumTest < ActiveSupport::TestCase
-#    def setup
-#   	@museumFalse = Museum.new(name: "Whitney Museum")
-#   	@museum = Museum.new(name: "Whitney Museum", location: "New York, NY")
-#   end
+class MuseumTest < ActiveSupport::TestCase
+   def setup
+  	@museumFalse = Museum.new()
+  	@museum = museums(:moad)
+  	@exhibit2 = exhibitions(:exhibit2)
+  end
 
-#   test "should not validate if museum name or location are missing" do
-#   	assert_not @museumFalse.valid?
-#   end
+  test "should not validate if museum name or location are missing" do
+  	assert_not @museumFalse.valid?
+  end
 
-#   test "should validate museum if name and location are provided" do
-#   	assert @museum.valid?
-#   end
+  test "should validate museum if name, location, and city association are provided" do
+  	assert @museum.save
+  end
+
+  test "should have an exhibition association" do
+  	assert_equal [@exhibit2], @museum.exhibitions
+  end
+
+  test "should belong to a city" do
+  	# print "Belong to city: #{@museum.city.name}"
+  	assert_equal "San Francisco", @museum.city.name
+  end
  
-# end
+end
